919758726a7a35e11f76d6a5bb3501d235e55d74
devy52 <108571763+devy52@users.noreply.github.com>
2025-03-25 19:52:17 +0530

    Update __init__.py

--- basicsr/__init__.py
+++ basicsr/__init__.py
@@ -1,10 +1,10 @@
 # https://github.com/xinntao/BasicSR
 # flake8: noqa
-#from .archs import *
-#from .data import *
-#from .losses import *
-#from .metrics import *
-#from .models import *
+from .archs import *
+from .data import *
+from .losses import *
+from .metrics import *
+from .models import *
 from .ops import *
 from .test import *
 from .train import *
