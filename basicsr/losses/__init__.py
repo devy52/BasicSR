1a2fdfd9292f8dc92a293d34b2caab74050b4c7b
devy52 <108571763+devy52@users.noreply.github.com>
2025-03-25 19:54:22 +0530

    Update __init__.py

--- basicsr/losses/__init__.py
+++ basicsr/losses/__init__.py
@@ -1,43 +1,31 @@
-# basicsr/losses/__init__.py
 import importlib
 from copy import deepcopy
 from os import path as osp
 
 from basicsr.utils import get_root_logger, scandir
 from basicsr.utils.registry import LOSS_REGISTRY
+from .gan_loss import g_path_regularize, gradient_penalty_loss, r1_penalty
 
-__all__ = ['build_loss']
-__all__ = ['build_loss']
+__all__ = ['build_loss', 'gradient_penalty_loss', 'r1_penalty', 'g_path_regularize']
 
-# List module names without importing them
+# automatically scan and import loss modules for registry
+# scan all the files under the 'losses' folder and collect files ending with '_loss.py'
 loss_folder = osp.dirname(osp.abspath(__file__))
 loss_filenames = [osp.splitext(osp.basename(v))[0] for v in scandir(loss_folder) if v.endswith('_loss.py')]
-_loss_modules = [f'basicsr.losses.{file_name}' for file_name in loss_filenames]
+# import all the loss modules
+_model_modules = [importlib.import_module(f'basicsr.losses.{file_name}') for file_name in loss_filenames]
+
 
 def build_loss(opt):
+    """Build loss from options.
+
+    Args:
+        opt (dict): Configuration. It must contain:
+            type (str): Model type.
+    """
     opt = deepcopy(opt)
-    loss = LOSS_REGISTRY.get(opt['type'])(**opt)
+    loss_type = opt.pop('type')
+    loss = LOSS_REGISTRY.get(loss_type)(**opt)
     logger = get_root_logger()
     logger.info(f'Loss [{loss.__class__.__name__}] is created.')
     return loss
-
-def dynamic_instantiation(modules, cls_type, opt):
-    for module_name in modules:
-        module = importlib.import_module(module_name)
-        cls_ = getattr(module, cls_type, None)
-        if cls_ is not None:
-            break
-    if cls_ is None:
-        raise ValueError(f'{cls_type} is not found.')
-    return cls_(**opt)
-
-# Register any explicitly imported loss
-def __getattr__(name):
-    if name in [f.replace('_loss', '') for f in loss_filenames]:
-        module_name = f'basicsr.losses.{name}_loss'
-        module = importlib.import_module(module_name)
-        cls_ = getattr(module, name, None)
-        if cls_ is not None:
-            return cls_
-        raise AttributeError(f"'{name}' not found in {module_name}")
-    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
