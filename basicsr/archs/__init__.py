b6c314d4f0de558144e16549879dff5958fbebef
devy52 <108571763+devy52@users.noreply.github.com>
2025-03-25 14:44:43 +0530

    Update __init__.py

--- basicsr/archs/__init__.py
+++ basicsr/archs/__init__.py
@@ -26,4 +26,25 @@ def build_network(opt):
 def define_network(opt):
     network_type = opt.pop('type')
     net = dynamic_instantiation(_arch_modules, network_type, opt)
-    return net
+    return netreturn net
+
+def dynamic_instantiation(modules, cls_type, opt):
+    """Dynamically instantiate class.
+
+    Args:
+        modules (list[importlib modules]): List of modules from importlib
+            files.
+        cls_type (str): Class type.
+        opt (dict): Class initialization kwargs.
+
+    Returns:
+        class: Instantiated class.
+    """
+
+    for module in modules:
+        cls_ = getattr(module, cls_type, None)
+        if cls_ is not None:
+            break
+    if cls_ is None:
+        raise ValueError(f'{cls_type} is not found.')
+    return cls_(**opt)
