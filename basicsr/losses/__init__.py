0cbb96d0745c1ba1a9e993d227e374059f28d760
devy52 <108571763+devy52@users.noreply.github.com>
2025-03-25 16:56:53 +0530

    Update __init__.py

--- basicsr/losses/__init__.py
+++ basicsr/losses/__init__.py
@@ -1,31 +1,37 @@
+# basicsr/losses/__init__.py
 import importlib
 from copy import deepcopy
 from os import path as osp
 
 from basicsr.utils import get_root_logger, scandir
 from basicsr.utils.registry import LOSS_REGISTRY
-from .gan_loss import g_path_regularize, gradient_penalty_loss, r1_penalty
 
-__all__ = ['build_loss', 'gradient_penalty_loss', 'r1_penalty', 'g_path_regularize']
+__all__ = ['build_loss']
 
-# automatically scan and import loss modules for registry
-# scan all the files under the 'losses' folder and collect files ending with '_loss.py'
+# List module names without importing them
 loss_folder = osp.dirname(osp.abspath(__file__))
 loss_filenames = [osp.splitext(osp.basename(v))[0] for v in scandir(loss_folder) if v.endswith('_loss.py')]
-# import all the loss modules
-_model_modules = [f'basicsr.losses.{file_name}' for file_name in loss_filenames]
-
+_loss_modules = [f'basicsr.losses.{file_name}' for file_name in loss_filenames]
 
 def build_loss(opt):
     """Build loss from options.
 
     Args:
         opt (dict): Configuration. It must contain:
-            type (str): Model type.
+            type (str): Loss type.
     """
     opt = deepcopy(opt)
-    loss_type = opt.pop('type')
-    loss = LOSS_REGISTRY.get(loss_type)(**opt)
+    loss = LOSS_REGISTRY.get(opt['type'])(**opt)
     logger = get_root_logger()
     logger.info(f'Loss [{loss.__class__.__name__}] is created.')
     return loss
+
+def dynamic_instantiation(modules, cls_type, opt):
+    for module_name in modules:
+        module = importlib.import_module(module_name)
+        cls_ = getattr(module, cls_type, None)
+        if cls_ is not None:
+            break
+    if cls_ is None:
+        raise ValueError(f'{cls_type} is not found.')
+    return cls_(**opt)
