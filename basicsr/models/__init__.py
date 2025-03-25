c9546449b9b9aec94aea16e58e49ebc8a9aba2d9
devy52 <108571763+devy52@users.noreply.github.com>
2025-03-25 19:54:58 +0530

    Update __init__.py

--- basicsr/models/__init__.py
+++ basicsr/models/__init__.py
@@ -1,4 +1,3 @@
-# basicsr/models/__init__.py
 import importlib
 from copy import deepcopy
 from os import path as osp
@@ -7,39 +6,24 @@ from basicsr.utils import get_root_logger, scandir
 from basicsr.utils.registry import MODEL_REGISTRY
 
 __all__ = ['build_model']
-__all__ = ['build_model']
-__all__ = ['build_model']
-__all__ = ['build_model']
 
-# List module names without importing them
+# automatically scan and import model modules for registry
+# scan all the files under the 'models' folder and collect files ending with '_model.py'
 model_folder = osp.dirname(osp.abspath(__file__))
 model_filenames = [osp.splitext(osp.basename(v))[0] for v in scandir(model_folder) if v.endswith('_model.py')]
-_model_modules = [f'basicsr.models.{file_name}' for file_name in model_filenames]
+# import all the model modules
+_model_modules = [importlib.import_module(f'basicsr.models.{file_name}') for file_name in model_filenames]
+
 
 def build_model(opt):
+    """Build model from options.
+
+    Args:
+        opt (dict): Configuration. It must contain:
+            model_type (str): Model type.
+    """
     opt = deepcopy(opt)
     model = MODEL_REGISTRY.get(opt['model_type'])(opt)
     logger = get_root_logger()
     logger.info(f'Model [{model.__class__.__name__}] is created.')
     return model
-
-def dynamic_instantiation(modules, cls_type, opt):
-    for module_name in modules:
-        module = importlib.import_module(module_name)
-        cls_ = getattr(module, cls_type, None)
-        if cls_ is not None:
-            break
-    if cls_ is None:
-        raise ValueError(f'{cls_type} is not found.')
-    return cls_(**opt)
-
-# Register any explicitly imported model
-def __getattr__(name):
-    if name in [f.replace('_model', '') for f in model_filenames]:
-        module_name = f'basicsr.models.{name}_model'
-        module = importlib.import_module(module_name)
-        cls_ = getattr(module, name, None)
-        if cls_ is not None:
-            return cls_
-        raise AttributeError(f"'{name}' not found in {module_name}")
-    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
