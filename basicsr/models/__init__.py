e19e0c9e4404211afcad2f07c307247c1db69abf
devy52 <108571763+devy52@users.noreply.github.com>
2025-03-25 20:00:44 +0530

    Update __init__.py

--- basicsr/models/__init__.py
+++ basicsr/models/__init__.py
@@ -27,3 +27,27 @@ def build_model(opt):
     logger = get_root_logger()
     logger.info(f'Model [{model.__class__.__name__}] is created.')
     return model
+
+
+def create_model(opt):
+    """Create model.
+
+    Args:
+        opt (dict): Configuration. It constains:
+            model_type (str): Model type.
+    """
+    model_type = opt['model_type']
+
+    # dynamic instantiation
+    for module in _model_modules:
+        model_cls = getattr(module, model_type, None)
+        if model_cls is not None:
+            break
+    if model_cls is None:
+        raise ValueError(f'Model {model_type} is not found.')
+
+    model = model_cls(opt)
+
+    logger = get_root_logger()
+    logger.info(f'Model [{model.__class__.__name__}] is created.')
+    return model
