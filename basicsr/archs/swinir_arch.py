3a7b1c8b64f01e4f24545ff071f5c1ee954a174a
devy52 <108571763+devy52@users.noreply.github.com>
2025-03-27 11:49:32 +0530

    Update swinir_arch.py

--- basicsr/archs/swinir_arch.py
+++ basicsr/archs/swinir_arch.py
@@ -29,7 +29,7 @@ def drop_path(x, drop_prob: float = 0., training: bool = False):
 class DropPath(nn.Module):
     """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
 
-    From: https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/layers/drop.py
+    From: https://github.com/rwightman/pytorch-image-models/blob/master/timm/layers/drop.py
     """
 
     def __init__(self, drop_prob=None):
